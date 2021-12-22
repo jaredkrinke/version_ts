349793d9d6e954c3759844b4722164c72a7b1de6
Jared Krinke <jared.krinke@gmail.com>
2021-12-22 13:55:38 -0800

    Simplify main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,25 @@
-import { version as verionTSVersion } from "./version.ts";
-
+import { version as versionTSVersion } from "./version.ts";
 import {
-    fileToString,
+    getCurrentVersionAsync,
     incrementVersion,
-    stringToFile,
+    saveVersionAsync,
+    tagVersionAsync,
     stringToVersion,
     versionToString,
-    Version,
 } from "./mod.ts";
-
 import {
     logUsage,
     processFlags,
 } from "https://deno.land/x/flags_usage@2.0.0/mod.ts";
 
 const flagInfo = {
-    preamble: "Usage: version_ts <major|minor|patch> | <options>",
+    preamble: "Usage: version_ts [options] [major|minor|patch]",
     description: {
         "increment": "Increments the specified version component",
         "set": "Overwrites the existing version with the specified version",
         "get": "Prints the current version",
         "tag": "Adds a Git tag for the current version",
-        "version": "Prints the version of version_ts (not the current module!)",
+        "version": "Prints the version of version_ts itself (not the current module!)",
     },
     argument: {
         "increment": "major|minor|patch",
@@ -44,75 +42,51 @@ const flagInfo = {
     ],
 };
 
+const defaultVersionString = "1.0.0";
 const flags = processFlags(Deno.args, flagInfo);
 try {
     if (flags._.length > 1) {
         throw `Expected 0 or 1 arguments, but got ${flags._.length}`;
     }
 
-    if (flags._.length === 1) {
-        flags["increment"] = flags._[0];
-    }
-
-    let update = false; // True if version.ts needs to be overwritten
-
-    // --set, otherwise read the current version, defaulting to 1.0.0
-    let version: Version;
-    if (flags.set) {
-        version = stringToVersion(flags.set);
-        update = true;
-    } else {
-        // Use default value if no previous version exists
-        let text = stringToFile("1.0.0");
-        try {
-            text = await Deno.readTextFile("version.ts");
-        } catch (error) {
-            // Let permission errors bubble up
-            if (error instanceof Deno.errors.PermissionDenied) {
-                throw error;
-            }
-        }
-    
-        version = stringToVersion(fileToString(text));
-    }
-
-    // --increment
-    const component: string = flags["increment"];
-    if (component) {
-        switch (component) {
-            case "major":
-            case "minor":
-            case "patch": {
-                version = incrementVersion(version, component);
-                update = true;
-                break;
-            }
-
-            default:
-                throw `Version component must be major|minor|patch, but got: ${component}`;
-        }
-    }
-
-    // --get
-    if (flags.get) {
-        console.log(versionToString(version));
-    }
-
-    // --tag
-    if (flags.tag) {
-        console.log(`git tag ${versionToString(version)}`);
-    }
-
-    // --version (of version_ts -- NOT the current module)
+    // --version (of version_ts itself!)
     if (flags.version) {
-        console.log(verionTSVersion);
+        console.log(`version_ts version: ${versionTSVersion}`);
     }
 
-    // Update version.ts, if needed
-    if (update) {
-        await Deno.writeTextFile("version.ts", stringToFile(versionToString(version)));
+    if (flags.set || flags.increment || flags._[0] || flags.get || flags.tag) {
+        // Read "version.ts" or use default
+        const originalVersion = (await getCurrentVersionAsync());
+        let version = originalVersion ?? stringToVersion(defaultVersionString);
+
+        // --set
+        if (flags.set) {
+            version = stringToVersion(flags.set);
+        }
+
+        // --increment
+        const component: string = flags.increment ?? flags._[0];
+        if (component) {
+            version = incrementVersion(version, component);
+        }
+
+        // --get
+        if (flags.get) {
+            console.log(versionToString(version));
+        }
+
+        // Update "version.ts" *before* logging any "git tag" command (to prevent confusion about ordering)
+        if (version !== originalVersion) {
+            await saveVersionAsync(version);
+            console.log(`Updated version from ${originalVersion ? versionToString(originalVersion) : "(none)"} to ${versionToString(version)}`);
+        }
+
+        // --tag
+        if (flags.tag) {
+            await tagVersionAsync(version);
+        }
     }
 } catch (error) {
     console.log(`Error: ${error.toString()}`);
-    console.log(logUsage(flagInfo));
+    logUsage(flagInfo);
 }
--- mod.ts
+++ mod.ts
@@ -1,54 +1,95 @@
-export interface Version {
-    major: number;
-    minor: number;
-    patch: number;
-}
+// Model
+const versionComponentIndexes = {
+    "major": 0,
+    "minor": 1,
+    "patch": 2,
+} as const;
 
+type VersionComponent = keyof typeof versionComponentIndexes;
+export type Version = { [ Property in VersionComponent ]: number };
+
+// Parsing and encoding
 const versionPattern = /^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$/;
 export function stringToVersion(versionString: string): Version {
     const components = versionPattern.exec(versionString);
-    if (!components) {
-        throw `Invalid version string: ${versionString}`;
+    if (components) {
+        return {
+            major: parseInt(components[1]),
+            minor: parseInt(components[2]),
+            patch: parseInt(components[3]),
+        };
     }
-
-    return {
-        major: parseInt(components[1]),
-        minor: parseInt(components[2]),
-        patch: parseInt(components[3]),
-    };
+    throw `Invalid version string: ${versionString}`;
 }
 
 export function versionToString(version: Version): string {
     return `${version.major}.${version.minor}.${version.patch}`;
 }
 
-const versionFilePattern = /^export const version = "((0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*))";$/
-export function fileToString(text: string): string {
-    const matches = versionFilePattern.exec(text);
-    if (!matches) {
-        throw `Invalid version.ts: ${text}`;
+const typeScriptPattern = /^export const version = "((0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*))";$/
+export function typeScriptToVersion(typeScript: string): Version {
+    const matches = typeScriptPattern.exec(typeScript);
+    if (matches) {
+        return stringToVersion(matches[1]);
     }
-    return matches[1];
+    throw `Invalid version.ts: ${typeScript}`;
 }
 
-export function stringToFile(versionString: string): string {
-    return `export const version = "${versionString}";`
+export function versionToTypeScript(version: Version): string {
+    return `export const version = "${versionToString(version)}";`
 }
 
-const componentIndex = {
-    "major": 0,
-    "minor": 1,
-    "patch": 2,
-};
+// Manipulation
+export function incrementVersion(currentVersion: Version, component: string): Version {
+    switch (component) {
+        case "major":
+        case "minor":
+        case "patch": {
+            const components = [currentVersion.major, currentVersion.minor, currentVersion.patch];
+            const index = versionComponentIndexes[component];
+            components[index]++;
+            for (let i = index + 1; i < components.length; i++) {
+                components[i] = 0;
+            }
+        
+            const [major, minor, patch] = components;
+            return { major, minor, patch };
+        }
 
-export function incrementVersion(currentVersion: Version, component: "major" | "minor" | "patch"): Version {
-    const components = [currentVersion.major, currentVersion.minor, currentVersion.patch];
-    const index = componentIndex[component];
-    components[index]++;
-    for (let i = index + 1; i < components.length; i++) {
-        components[i] = 0;
+        default:
+            throw `Version component must be ${Object.keys(versionComponentIndexes).join("|")}, but got: ${component}`;
+    }
+}
+
+// File handling
+const fileName = "version.ts";
+export async function getCurrentVersionAsync(): Promise<Version | undefined> {
+    // Use default value if no previous version exists
+    let text: string;
+    try {
+        text = await Deno.readTextFile(fileName);
+    } catch (error) {
+        if (error instanceof Deno.errors.NotFound) {
+            return undefined;
+        }
+        throw error;
     }
 
-    const [major, minor, patch] = components;
-    return { major, minor, patch };
+    return typeScriptToVersion(text);
+}
+
+export async function saveVersionAsync(version: Version): Promise<void> {
+    await Deno.writeTextFile(fileName, versionToTypeScript(version));
+}
+
+// Tagging
+export async function tagVersionAsync(version: Version): Promise<void> {
+    const command = `git tag ${versionToString(version)}`;
+    console.log(`Running command: ${command}`);
+    const process = Deno.run({
+        cmd: command.split(" "),
+        stdin: "null",
+    });
+
+    await process.status();
 }
